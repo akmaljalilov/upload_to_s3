c8656a7b1dc3adc74bc582aaa295436e52564a27
Akmaljon <ajalilov@transparent.com>
2021-09-17 18:43:27 +0500

    d

--- docker_build.sh
+++ docker_build.sh
@@ -3,5 +3,6 @@ DOCKER_LATEST=test
 docker build -t $DOCKER_LATEST .
 CONTAINER_COPY=${DOCKER_LATEST}_container
 docker run --name $CONTAINER_COPY -d -t $DOCKER_LATEST
+mkdir dist
 docker cp $CONTAINER_COPY:/build/hi.txt ./dist
 docker rm $CONTAINER_COPY -f
