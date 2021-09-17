96eb6177fd88f9c58dd4206e81caf6c461021fdb
Akmaljon <ajalilov@transparent.com>
2021-09-17 18:56:30 +0500

    d

--- docker_build.sh
+++ docker_build.sh
@@ -3,5 +3,6 @@ DOCKER_LATEST=test
 docker build -t $DOCKER_LATEST .
 CONTAINER_COPY=${DOCKER_LATEST}_container
 docker run --name $CONTAINER_COPY -d -t $DOCKER_LATEST
+mkdir -p Yes
 docker cp $CONTAINER_COPY:/build/hi.txt ./Yes/yra.txt
 docker rm $CONTAINER_COPY -f
