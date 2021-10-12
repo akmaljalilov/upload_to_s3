c35d14399303741c14d1069a9103f595a45fe667
Akmaljon <ajalilov@transparent.com>
2021-10-12 14:11:35 +0500

    d

---
+++ images/docker_build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Arguments by Position
+# 1 - Version Tag (example: 1.0.3-master)
+# 2 - Dockerfile location
+# 3 - Project registry repo name (example: daise)
+# 4 - Registry name (default: 711226717742.dkr.ecr.us-east-1.amazonaws.com)
+#
+# Full Registry string: <registry_name>/<project_repo_name>:<version_tag>
+
+#VERSION=${1:-0.0.0}
+
+DOCKERFILE_LOC=${2:-Dockerfile}
+PROJECT_NAME=${3:-flutter}
+FLUTTER_TAG=${4:-2.5.2}
+REGISTRY_NAME=${5:-711226717742.dkr.ecr.us-east-1.amazonaws.com}
+DOCKER_BUILD=$REGISTRY_NAME/$PROJECT_NAME:$FLUTTER_TAG
+#DOCKER_LATEST=$REGISTRY_NAME/$PROJECT_NAME:latest
+
+echo "Running as"
+whoami
+
+#echo "Moving Dockerfile"
+#echo $DOCKERFILE_LOC
+#cp $DOCKERFILE_LOC Dockerfile
+
+echo "Building and Pushing docker image"
+echo $DOCKER_BUILD
+docker build -t $DOCKER_BUILD -f $DOCKERFILE_LOC --build-arg FLUTTER_TAG=$FLUTTER_TAG .
+#docker push $DOCKER_BUILD
+
+#echo "Building and Pushing docker latest"
+#echo $DOCKER_LATEST
+#docker build -t $DOCKER_LATEST .
+#docker push $DOCKER_LATEST
+
+echo "Cleaning local images"
+docker rmi $DOCKER_BUILD
+#docker rmi $DOCKER_LATEST
